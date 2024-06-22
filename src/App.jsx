9271e444608623f6796ecfb6e7d02d92c127cd21
phanduyan2511@gmail.com <124804965+PhanDuyAn2511@users.noreply.github.com>
2024-06-22 23:19:07 +0700

    Refactor Project Structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import './App.css'
-import Button from '@mui/material/Button'
+import Board from './pages/Boards/_id'
 
 function App() {
 
   return (
     <>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+      <Board/>
     </>
   )
 }
