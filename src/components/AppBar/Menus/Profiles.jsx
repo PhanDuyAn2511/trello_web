5fb91759cbdde406174417a1a9cd98222b9c0e0c
phanduyan2511@gmail.com <124804965+PhanDuyAn2511@users.noreply.github.com>
2024-06-23 10:00:43 +0700

    Theme_Override

---
+++ src/components/AppBar/Menus/Profiles.jsx
@@ -0,0 +1,74 @@
+
+import React from 'react'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Divider from '@mui/material/Divider'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import Avatar from '@mui/material/Avatar';
+import { IconButton, Tooltip } from '@mui/material'
+import { Logout, PersonAdd, Settings } from '@mui/icons-material'
+
+
+function Profiles() {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  return (
+    <div>
+      <Tooltip title="Account settings">
+        <IconButton
+          onClick={handleClick}
+          size="small"
+          sx={{ padding: 0 }}
+          aria-controls={open ? 'basic-menu-profiles' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+        >
+          <Avatar sx={{ width: 30, height: 30 }}>A</Avatar>
+        </IconButton>
+      </Tooltip>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button'
+        }}
+      >
+        <MenuItem >
+          <Avatar sx={{ width: 28, height: 28, mr: 2}} /> Profile
+        </MenuItem>
+        <MenuItem >
+          <Avatar sx={{ width: 28, height: 28, mr: 2 }} /> My account
+        </MenuItem>
+        <Divider />
+        <MenuItem >
+          <ListItemIcon>
+            <PersonAdd fontSize="small" />
+          </ListItemIcon>
+          Add another account
+        </MenuItem>
+        <MenuItem >
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem >
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </div>
+  )
+}
+
+export default Profiles
